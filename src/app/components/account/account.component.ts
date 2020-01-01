03725c15b5dbf7a8f1d7878ac5695acb15ba0773
filiploskovski <filiploskovski11@gmail.com>
2020-01-01 20:08:42 +0100

    added datatable with bt4 support and modal box for delete

--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -2,21 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 
-class Person {
-  id: number;
-  firstName: string;
-  lastName: string;
-}
-
-class DataTablesResponse {
-  data: any[];
-  draw: number;
-  recordsFiltered: number;
-  recordsTotal: number;
-}
-
-
-
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
@@ -25,8 +10,6 @@ class DataTablesResponse {
 export class AccountComponent implements OnInit {
 
   dtOptions: DataTables.Settings = {};
-  persons: Person[];
-
 
   accountForm = new FormGroup({
     accountType: new FormControl('', [Validators.required]),
@@ -37,29 +20,8 @@ export class AccountComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    const that = this;
-
     this.dtOptions = {
-      pagingType: 'full_numbers',
-      pageLength: 2,
-      serverSide: true,
-      processing: true,
-      ajax: (dataTablesParameters: any, callback) => {
-        that.http
-          .post<DataTablesResponse>(
-            'https://angular-datatables-demo-server.herokuapp.com/',
-            dataTablesParameters, {}
-          ).subscribe(resp => {
-            that.persons = resp.data;
-
-            callback({
-              recordsTotal: resp.recordsTotal,
-              recordsFiltered: resp.recordsFiltered,
-              data: []
-            });
-          });
-      },
-      columns: [{ data: 'id' }, { data: 'firstName' }, { data: 'lastName' }]
+      pagingType: 'full_numbers'
     };
   }
 
