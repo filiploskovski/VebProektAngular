c8b9e4eeac7cb5a08e29c36866ccc8b966c7e7c9
filiploskovski <filiploskovski11@gmail.com>
2020-01-06 02:30:49 +0100

    added logic to init model

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AccountModel } from 'src/app/models/AccountModel';
+import { ApiService } from 'src/app/services/api.service';
+import { AccountTypeModel } from 'src/app/models/AccountTypeModel';
+import { DataTransferService } from 'src/app/services/data-transfer.service';
 
 @Component({
   selector: 'app-home',
@@ -8,13 +12,24 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+
+  public accountList: AccountModel[];
+
+  constructor(private router: Router,private api: ApiService,private dataTransfer: DataTransferService) { }
 
   ngOnInit() {
+    this.getAccount();
   }
 
-  accountDetails(){
+  accountDetails(id: number){
+    this.dataTransfer.setMessage(id);
     this.router.navigate(['/account-details']);
+  }
+
+  getAccount(): void {
+    this.api.AccountGet().subscribe({
+        next: (model: AccountModel[]) => this.accountList = model
+    });
   }
 
 }
